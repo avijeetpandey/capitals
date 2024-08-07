e367539a080274b8baced8cf999e8de6a201a5d2
Avijeet Pandey <avijeetpandey25@gmail.com>
2024-08-07 14:05:27 +0530

    refactor: application code

---
+++ lib/root.dart
@@ -0,0 +1,19 @@
+import 'package:capitals/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class CapitalsApp extends StatelessWidget {
+  const CapitalsApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
