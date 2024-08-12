c82afe80a75e3d66e2e3ef2bc6417b42ff388e66
Avijeet Pandey <avijeetpandey25@gmail.com>
2024-08-13 02:11:10 +0530

    add: more questions

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -21,51 +21,78 @@ final List<QuizQuestion> questionnaire = [
       IndianCapital.MEGHALAYA
     ],
   ),
-  // QuizQuestion(
-  //   question: "What is the capital of ${IndianState.ARUNACHAL}?",
-  //   answers: [
-  //     IndianCapital.ARUNACHAL,
-  //     IndianCapital.MANIPUR,
-  //     IndianCapital.KARNATAKA,
-  //     IndianCapital.DELHI
-  //   ],
-  // ),
-  // QuizQuestion(
-  //   question: "What is the capital of ${IndianState.ASSAM}?",
-  //   answers: [
-  //     IndianCapital.ASSAM,
-  //     IndianCapital.JHARKHAND,
-  //     IndianCapital.GOA,
-  //     IndianCapital.DELHI
-  //   ],
-  // ),
-  // QuizQuestion(
-  //   question: "What is the capital of ${IndianState.BIHAR}?",
-  //   answers: [
-  //     IndianCapital.BIHAR,
-  //     IndianCapital.MAHARASHTRA,
-  //     IndianCapital.MP,
-  //     IndianCapital.JHARKHAND
-  //   ],
-  // ),
-  // QuizQuestion(
-  //   question: "What is the capital of ${IndianState.CHATTISGARH}?",
-  //   answers: [
-  //     IndianCapital.CHATTISGARH,
-  //     IndianCapital.HARYANA,
-  //     IndianCapital.TRIPURA,
-  //     IndianCapital.PUNJAB
-  //   ],
-  // ),
-  // QuizQuestion(
-  //   question: "What is the capital of ${IndianState.GOA}?",
-  //   answers: [
-  //     IndianCapital.GOA,
-  //     IndianCapital.MANIPUR,
-  //     IndianCapital.RAJASTHAN,
-  //     IndianCapital.NAGALAND
-  //   ],
-  // ),
+  QuizQuestion(
+    question: "What is the capital of ${IndianState.ARUNACHAL}?",
+    answers: [
+      IndianCapital.ARUNACHAL,
+      IndianCapital.MANIPUR,
+      IndianCapital.KARNATAKA,
+      IndianCapital.DELHI
+    ],
+  ),
+  QuizQuestion(
+    question: "What is the capital of ${IndianState.ASSAM}?",
+    answers: [
+      IndianCapital.ASSAM,
+      IndianCapital.JHARKHAND,
+      IndianCapital.GOA,
+      IndianCapital.DELHI
+    ],
+  ),
+  QuizQuestion(
+    question: "What is the capital of ${IndianState.BIHAR}?",
+    answers: [
+      IndianCapital.BIHAR,
+      IndianCapital.MAHARASHTRA,
+      IndianCapital.MP,
+      IndianCapital.JHARKHAND
+    ],
+  ),
+  QuizQuestion(
+    question: "What is the capital of ${IndianState.CHATTISGARH}?",
+    answers: [
+      IndianCapital.CHATTISGARH,
+      IndianCapital.HARYANA,
+      IndianCapital.TRIPURA,
+      IndianCapital.PUNJAB
+    ],
+  ),
+  QuizQuestion(
+    question: "What is the capital of ${IndianState.GOA}?",
+    answers: [
+      IndianCapital.GOA,
+      IndianCapital.MANIPUR,
+      IndianCapital.RAJASTHAN,
+      IndianCapital.NAGALAND
+    ],
+  ),
+  QuizQuestion(
+    question: "What is the capital of ${IndianState.GUJARAT}?",
+    answers: [
+      IndianCapital.GUJARAT,
+      IndianCapital.SIKKIM,
+      IndianCapital.MAHARASHTRA,
+      IndianCapital.UP
+    ],
+  ),
+  QuizQuestion(
+    question: "What is the capital of ${IndianState.HARYANA}?",
+    answers: [
+      IndianCapital.HARYANA,
+      IndianCapital.HIMACHAL,
+      IndianCapital.ODISHA,
+      IndianCapital.WB
+    ],
+  ),
+  QuizQuestion(
+    question: "What is the capital of ${IndianState.HIMACHAL}?",
+    answers: [
+      IndianCapital.HIMACHAL,
+      IndianCapital.BIHAR,
+      IndianCapital.TAMILNADU,
+      IndianCapital.TELENGANA
+    ],
+  ),
 ];
 
 final dummyQuestionare = QuizQuestion(
